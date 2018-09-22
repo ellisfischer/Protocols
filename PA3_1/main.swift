daf757a3af06e6c2d90f50127ca244ef0b0069cd
trina fischer <trinafischer@yahoo.com>
2018-09-22 11:40:43 -0700

    Initial Commit

---
+++ PA3_1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PA3_1
+//
+//  Created by Michael Fischer on 9/22/18.
+//  Copyright © 2018 Ellis Fischer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
